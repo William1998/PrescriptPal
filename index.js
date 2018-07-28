f00418b3a48283ad93f61943295b7026f033354b
Tomo Suzuki <tsuz15@student.monash.edu>
2018-07-29 01:20:13 +1000

    First page working!

--- index.js
+++ index.js
@@ -4,20 +4,30 @@ var app = express()
 var MongoClient = require('mongodb').MongoClient,
   test = require('assert');
 var url = 'mongodb+srv://admin:1234@cluster0-mr1r8.mongodb.net/PrescriptPal?retryWrites=true'
+var path = require('path');
 
 var server = app.listen(8080, function(){
   console.log('listening to requests on 8080');
 })
 
-app.get('/', (req, res)=> {
+app.use(express.static(path.join(__dirname, '/public')))
+
+app.get('/', (req, res) => {
+  console.log('home reached');
+  res.sendFile(path.join(__dirname + "/public/index.html"));
+})
+
+/*
+app.get('/search', (req, res)=> {
   console.log('endpoint reached');
   MongoClient.connect(url, function(err, db){
     if (err) throw err;
     var dbo = db.db('PrescriptPal');
     dbo.collection("Medicines").findOne({}, function(err, result){
       if (err) throw err;
-      console.log(result.name);
+      console.log(result);
       db.close();
     });
   });
 })
+*/
